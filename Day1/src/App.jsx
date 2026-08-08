5f85027a00d520b509e39e3d5402754abf777092
Rukiya Nus Ha <rukiyanusha123@gmail.com>
2026-08-08 21:20:40 +0530

    Day 1 - useReducer counter and Axios JSON server

---
+++ Day1/src/App.jsx
@@ -0,0 +1,39 @@
+import { useReducer } from "react";
+
+const initialState={
+  count:0
+}
+function reducer(state,action){
+  switch(action.type){
+    case "increment":
+   return{
+     count:state.count+1
+   }
+   case "decrement":
+    return{
+      count:state.count-1
+    }
+
+    case "reset":
+      return{
+        count:0
+      }
+   
+      default :
+      return state;
+  };
+}
+function App(){
+  const[state,dispatch]=useReducer(
+    reducer,initialState
+  );
+  return(
+    <>
+        <h1>Count: {state.count}</h1>
+    <button onClick={()=>dispatch({type:"increment"})}>+</button>
+    <button onClick={()=>dispatch({type:"decrement"})}>-</button>
+    <button onClick={()=>dispatch({type:"reset"})}>RESET</button>
+    </>
+  )
+}
+export default App;
